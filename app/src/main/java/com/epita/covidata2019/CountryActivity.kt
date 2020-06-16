96ca9f31e7752f3f17845c22e585901fae8424f8
Bora MINDAS <bora.mindas@epita.fr>
2020-06-17 00:39:05 +0200

    Added graph screen, country screen and modified data screen

---
+++ app/src/main/java/com/epita/covidata2019/CountryActivity.kt
@@ -0,0 +1,11 @@
+package com.epita.covidata2019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CountryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_country_data)
+    }
+}
--- app/src/main/java/com/epita/covidata2019/MainActivity.kt
+++ app/src/main/java/com/epita/covidata2019/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.epita.covidata2019
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonGoData.setOnClickListener{
+            val intent = Intent(this, DataActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonGoGraph.setOnClickListener{
+            val intent = Intent(this, GraphActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
+
+
 }
